2ce6c88c18eb3510891424fe857e86e4af4cc5f9
Orkea <orkea@hotmail.fr>
2024-02-28 23:32:53 +0100

    Ajout des produits au panier  du local Storage (quantité+ ou produit)

--- front/js/product.js
+++ front/js/product.js
@@ -5,11 +5,10 @@ let productId = productUrl.searchParams.get("id")
 // Récupération des détails d'un produit
 const reponse = await fetch("http://localhost:3000/api/products/" + productId)
 const product = await reponse.json()
-
 // Récupération de l'élements du DOM qui acueillent le logo d'un canapé
 const logo = document.querySelector(".item__img")
 const logoProduct = document.createElement("img")
-logoProduct.src = "../images/logo.png"
+logoProduct.src = product.imageUrl
 logo.appendChild(logoProduct)
 // On rajoute le nom du canapé
 const titleProduct = document.getElementById("title")
@@ -23,21 +22,63 @@ descriptionProduct.innerText = product.description
 // On rajoute les couleurs
     // On recupere la liste des couleurs
     const colorsProduct = product.colors
-    console.log(colorsProduct)
+    // on récupere l'élement du DOM qui accueille les couleurs
+    const sectionColors = document.getElementById("colors")
     for(let i=0; i<colorsProduct.length; i++){
-        let value = colorsProduct[i]
-        console.log(value)
-        // on récupere l'élement du DOM qui accueille les couleurs
-        const sectionColors = document.getElementById("colors")
-        // On ajoute la balise option
+        // On ajoute la balise option avec sa couleur
         const optionProduct = document.createElement("option")
         optionProduct.value = colorsProduct[i]
         optionProduct.innerText = colorsProduct[i]
         sectionColors.appendChild(optionProduct)
-
-
     }
 
+
+// Gestion du produit au sein du panier   
+// Cette fonction permet d'enregister le panier dans le local Storage
+function saveBasket(product){
+    localStorage.setItem("Basket", JSON.stringify(product))
+}
+// Cette fonction permet de recupèrer le panier du local storage, s'il est vide on crée un array vide
+export function getBasket(){
+    let basket = localStorage.getItem ("Basket")
+    if (basket === null) {
+        return []   
+    } else {
+        return JSON.parse(basket)
+    }
+}
+// Cette fonction permet de rajouter le produit dans le panier ou augmente le quantité s'il existe
+function addBasket(product){
+    // On recupèrer le panier
+    let basket = getBasket()
+    let quantityProduct = document.getElementById("quantity").valueAsNumber
+    let colorProduct = document.getElementById("colors").value
+    // On cherche dans le panier si le produit existe
+    let foundProduct = basket.find(p => p.id === productId && p.colorProduct === colorProduct)
+    console.log("j'ai trouvé : " + foundProduct)
+    if (foundProduct !== undefined) {
+                foundProduct.quantityProduct+= quantityProduct
+            } else {
+                product.quantityProduct = quantityProduct
+                basket.push(product)
+            }
+    // On sauvegarde le nouveau panier
+    saveBasket(basket)
+}
+
+    // On recupèrer la balise du DOM qui gerer l'ajout au panier
+    const btnAddBasket = document.querySelector("button")
+    //On ecoute le click sur le btn "Ajouter au panier"
+    btnAddBasket.addEventListener("click", (event)=>{
+        event.preventDefault()
+        //On crée le produit à ajouter
+        const productBasket = {
+            id : productId,
+            colorProduct : document.getElementById("colors").value,
+            quantityProduct: document.getElementById("quantity").valueAsNumber
+        }
+        addBasket(productBasket)
+    })
 
 
 
--- front/js/script.js
+++ front/js/script.js
@@ -2,26 +2,26 @@
 const reponse = await fetch("http://localhost:3000/api/products")
 const products = await reponse.json()
 
-function genererProduits(products) {
+function generateProduct() {
+    // Récupération de l'élément du DOM qui accueillera les fiches produits
+    const sectionFichesProduits = document.querySelector(".items")
     for (let i = 0; i < products.length; i++){
-        const article = products[i];
-        // Récupération de l'élément du DOM qui accueillera les fiches produits
-        const sectionFichesProduits = document.querySelector(".items")
-        // // Création d’une balise dédiée au lien vers le produits
+        const product = products[i];
+        // Création d’une balise dédiée au lien vers le produits
         const lienProduits = document.createElement("a")
-        lienProduits.href = `./product.html?id=${article._id}`
+        lienProduits.href = `./product.html?id=${product._id}`
         // Création d’une balise dédiée à un produit
         const produitElement = document.createElement("article")
         
         // Création des balises
         const imageElement = document.createElement("img")
-        imageElement.src= article.imageUrl
-        imageElement.alt = article.altTxt
+        imageElement.src= product.imageUrl
+        imageElement.alt = product.altTxt
         const nomElement = document.createElement("h3")
-        nomElement.innerText = article.name
+        nomElement.innerText = product.name
         nomElement.classList.add("productName")
         const descriptionElement = document.createElement("p")
-        descriptionElement.innerText = article.description
+        descriptionElement.innerText = product.description
         descriptionElement.classList.add("productDescription")
 
         // On rattache la balise article a la section 
@@ -32,4 +32,4 @@ function genererProduits(products) {
         produitElement.appendChild(descriptionElement)
     }
 }
-genererProduits(products)
+generateProduct(products)
